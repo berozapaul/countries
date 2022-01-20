9ebd4bad32b9b4b0531f0b0c863c76188860e500
Beroza Paul <beroza@cefalo.com>
2022-01-20 23:48:58 +0600

    Added material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import {Container} from '@material-ui/core';
 import './App.css';
 
-function App() {
+
+/*
+ * Purpose: This is the main component to bootstrap the app.
+ *
+ * Version: 1.0
+ * Author: dev@example.com
+ */
+const App: React.FC = () => {
+  const [data, setData] = useState([]);
+
+  useEffect( () => {
+     console.log('here');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container maxWidth="md">
+          <Header />
+        <h1>this is count</h1>
+      </Container>
   );
-}
+};
 
 export default App;
